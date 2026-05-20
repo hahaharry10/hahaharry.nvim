17c63c67a54ffaf4b45208153916a26df557a920
Harry Nixon <hnixon@hahaharry.dev>
2026-05-20 11:38:33 +0100

    fix issue from lazy breaking change

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -19,7 +19,7 @@ return {
         { 'j-hui/fidget.nvim', opts = {} },
 
         -- Allows extra capabilities provided by blink.cmp
-        'saghen/blink.cmp',
+        { 'saghen/blink.cmp', dependencies = { 'saghen/blink.lib' } },
     },
     config = function()
         vim.api.nvim_create_autocmd('LspAttach', {
@@ -118,6 +118,7 @@ return {
         --  When you add blink.cmp, luasnip, etc. Neovim now has *more* capabilities.
         --  So, we create new capabilities with blink.cmp, and then broadcast that to the servers.
         local capabilities = require('blink.cmp').get_lsp_capabilities()
+        capabilities.textDocumentSync = vim.lsp.protocol.TextDocumentSyncKind.Full
 
         -- Enable the following language servers
         --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
